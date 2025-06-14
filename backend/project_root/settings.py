cc05edd001683940dab3bcac627cc621c92111ff
HomeSani <dfghjjhgfd311@gmail.com>
2025-06-14 19:53:47 +0300

    update env settijngs

--- backend/project_root/settings.py
+++ backend/project_root/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
