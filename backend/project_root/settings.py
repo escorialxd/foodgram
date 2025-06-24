91443cb2334e1347aeffcbe10ee048f1ea224fe5
escorialxd <escorialxd@gmail.com>
2025-06-24 21:25:56 +0300

    minor fixes after deploy2

--- backend/project_root/settings.py
+++ backend/project_root/settings.py
@@ -16,7 +16,7 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 
 # Application definition
